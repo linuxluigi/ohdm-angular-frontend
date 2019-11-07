d87b5d49f91736cf9a659cecaad1b226237a0300
Exler <steffen.exler@saxsys.de>
2019-11-07 15:10:20 +0100

    add mutiple basic components & add routing to them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,33 @@ import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MapComponent } from './map/map.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+   { path: '', component: MapComponent },
+   { path: 'about', component: AboutComponent },
+   { path: 'imprint', component: ImprintComponent },
+   { path: 'privacy', component: PrivacyComponent },
+ ];
 
 @NgModule({
    declarations: [
       AppComponent,
-      MapComponent
+      MapComponent,
+      HeaderComponent,
+      AboutComponent,
+      ImprintComponent,
+      PrivacyComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      NgbModule
+      NgbModule,
+      RouterModule.forRoot(appRoutes),
    ],
    providers: [],
    bootstrap: [
