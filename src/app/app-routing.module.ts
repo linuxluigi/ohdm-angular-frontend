357e6fbe0dc66caf95d6c870659b2e211e1a1888
Steffen Exler <steffen.exler@pm.me>
2020-05-13 11:54:34 +0200

    change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
+import { ImprintComponent } from "./imprint/imprint.component";
+import { MapComponent } from "./map/map.component";
+import { PrivacyComponent } from "./privacy/privacy.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "map/2017/1/1",
+    pathMatch: "full",
+  },
+  { path: "map/:year/:month/:day", component: MapComponent },
+  { path: "about", component: AboutComponent },
+  { path: "imprint", component: ImprintComponent },
+  { path: "privacy", component: PrivacyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MapComponent } from './map/map.component';
-import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
-import { ImprintComponent } from './imprint/imprint.component';
-import { PrivacyComponent } from './privacy/privacy.component';
-import { Routes, RouterModule } from '@angular/router';
-import { DatepickerComponent } from './header/datepicker/datepicker.component';
-import { FormsModule } from '@angular/forms';
-import { MapService } from './map-service/map.service';
-
-const today: Date = new Date();
-const appRoutes: Routes = [
-   { path: '', redirectTo: 'map/' + today.getFullYear() + '/1/1', pathMatch: 'full' },
-   { path: 'map/:year/:month/:day', component: MapComponent },
-   { path: 'about', component: AboutComponent },
-   { path: 'imprint', component: ImprintComponent },
-   { path: 'privacy', component: PrivacyComponent },
-];
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AboutComponent } from "./about/about.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DatepickerComponent } from "./header/datepicker/datepicker.component";
+import { HeaderComponent } from "./header/header.component";
+import { ImprintComponent } from "./imprint/imprint.component";
+import { MapService } from "./map-service/map.service";
+import { MapComponent } from "./map/map.component";
+import { PrivacyComponent } from "./privacy/privacy.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      MapComponent,
-      HeaderComponent,
-      AboutComponent,
-      ImprintComponent,
-      PrivacyComponent,
-      DatepickerComponent
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      NgbModule,
-      RouterModule.forRoot(appRoutes),
-      FormsModule,
-   ],
-   providers: [MapService],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    MapComponent,
+    HeaderComponent,
+    AboutComponent,
+    ImprintComponent,
+    PrivacyComponent,
+    DatepickerComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
+  providers: [MapService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
